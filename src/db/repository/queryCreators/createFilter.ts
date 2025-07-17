d7c998966902777d5dbf20b5c86ea99f5a5c0ce8
TvSpichkin <ericlen@mail.ru>
2025-07-17 17:20:54 +0300

    Добавление генерация фильтра

---
+++ src/db/repository/queryCreators/createFilter.ts
@@ -0,0 +1,22 @@
+import {Document, Filter, Sort} from "mongodb";
+import {EntDbType, KeysDB, ProtoFilterType, TypeEntFields} from "../types/typesRepDB";
+
+
+function valueAssigner(w: ProtoFilterType["way"], v: ProtoFilterType["value"]) {
+    switch(w) {
+        case 1:
+            return {$regex: v, $options: "i"};
+        default:
+            return v;
+    }
+} // Присваивание значений
+
+export function createFilter<T>(pf: ProtoFilterType[]): Filter<T> {
+    const f: Filter<T> = {};
+    
+    for(let i = 0; i < pf.length; i++) {
+        f[pf[i].key] = valueAssigner(pf[i].way, pf[i].value); // Заполнение фильтра
+    }
+    
+    return f;
+} // Генерация фильтра
