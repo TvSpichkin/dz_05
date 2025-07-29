79901d9f7b0d9f80b9d4a10b1ff9a0890c2a79fb
TvSpichkin <ericlen@mail.ru>
2025-07-29 16:38:44 +0300

    Подключение в контролёр, отвечающий за выдачу сетевых журналов, нумерации страниц и конвертации

--- src/routes/blogs/controllers/getBlogsController.ts
+++ src/routes/blogs/controllers/getBlogsController.ts
@@ -1,14 +1,21 @@
 import {Response} from "express";
 import {ReqQuery} from "../../types/reqTypes";
 import {QueryBlogInputModel} from "../types/queryBlogTypes";
-import {Paginator} from "../../present/paginator";
+import {Paginator, paginator} from "../../present/paginator";
 import {BlogViewModel, blogFields, TypeBlogFields} from "../types/blogsTypes";
-import { blogsQueryRep } from "../../../db/repository/blogs/blogsQueryRep";
+import {ProtoFilterType} from "../../../db/repository/types/typesRepDB";
+import {BlogDbType} from "../../../db/repository/types/blogsDbTypes";
+import {blogsQueryRep} from "../../../db/repository/blogs/blogsQueryRep";
+import {blogMaper} from "../../../db/mapers/blogMaper";
 
 
 export async function getBlogsController(req: ReqQuery<QueryBlogInputModel>, res: Response<Paginator<BlogViewModel>>) {
     const q = req.query,
-    sortBy = (blogFields.hasOwnProperty(q.sortBy) ? q.sortBy : blogFields.createdAt) as TypeBlogFields; // Задание исходного значения поля сортировки
+    elemsSkip = q.pageSize*(q.pageNumber - 1), // Количество пропущенных элементов
+    sortBy = q.sortBy, // Задание исходного значения поля сортировки as TypeBlogFields
+    searchNameFilt = q.searchNameTerm ? // Данные поискового термина для генерации фильтра
+        [{key: blogFields.name, value: q.searchNameTerm, way: 1}] : [],
+    [totalCount, blogs] = await blogsQueryRep.readAll(elemsSkip, q.pageSize, sortBy, q.sortDirection, searchNameFilt); // Получение сетевых журналов и их количества
     
-    res.json(await blogsQueryRep.readAll(q.searchNameTerm, sortBy, q.sortDirection, q.pageNumber, q.pageSize)); // Получение сетевых журналов
+    res.json(paginator(q.pageNumber, q.pageSize, totalCount, blogs.map(blogMaper))); // Нумерация страниц
 } // Контролёр, отвечающий за выдачу сетевых журналов
