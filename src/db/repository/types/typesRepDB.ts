c3230797ee3335244a2d07eed1594021c6a6a38f
TvSpichkin <ericlen@mail.ru>
2025-07-30 15:09:35 +0300

    Исправление типизации для фильтра

--- src/db/repository/types/typesRepDB.ts
+++ src/db/repository/types/typesRepDB.ts
@@ -1,5 +1,7 @@
+import {EnhancedOmit} from "mongodb";
 import {BlogDbPutType, BlogDbType, BlogDbTypeA} from "./blogsDbTypes";
 import {PostDbPutType, PostDbType, PostDbTypeA} from "./postsDbTypes";
+import { TypeBlogFields } from "../../../routes/blogs/types/blogsTypes";
 
 
 export type DBType = {
@@ -7,9 +9,7 @@ export type DBType = {
     posts: PostDbType[] // Массив записей
 }; // Типизация базы данных (что мы будем в ней хранить)
 export type ProtoFilterType<T> = {
-    key: keyof ((string extends keyof T ? T : T extends any ? Pick<T, never> : never) & {
-    id: never;
-}), // Поле сущности в БД
+    key: keyof ((string extends keyof T ? T : any) & {id: number}), // Поле сущности в БД !!! Не понимаю, как работает эта ужасная типизация
     // keyof WithId<T> // keyof EnhancedOmit<T, "_id">
     // keyof (string extends keyof T ? T : T extends any ? Pick<T, Exclude<keyof T, "_id">> : never)
     value: boolean | number | string, // Значение этого поля
--- src/routes/blogs/controllers/getBlogsController.ts
+++ src/routes/blogs/controllers/getBlogsController.ts
@@ -2,9 +2,7 @@ import {Response} from "express";
 import {ReqQuery} from "../../types/reqTypes";
 import {QueryBlogInputModel} from "../types/queryBlogTypes";
 import {Paginator, paginator} from "../../present/paginator";
-import {BlogViewModel, blogFields, TypeBlogFields} from "../types/blogsTypes";
-import {ProtoFilterType} from "../../../db/repository/types/typesRepDB";
-import {BlogDbType} from "../../../db/repository/types/blogsDbTypes";
+import {BlogViewModel, blogFields} from "../types/blogsTypes";
 import {blogsQueryRep} from "../../../db/repository/blogs/blogsQueryRep";
 import {blogMaper} from "../../../db/mapers/blogMaper";
 
@@ -12,9 +10,8 @@ import {blogMaper} from "../../../db/mapers/blogMaper";
 export async function getBlogsController(req: ReqQuery<QueryBlogInputModel>, res: Response<Paginator<BlogViewModel>>) {
     const q = req.query,
     elemsSkip = q.pageSize*(q.pageNumber - 1), // Количество пропущенных элементов
-    sortBy = q.sortBy, // Задание исходного значения поля сортировки as TypeBlogFields
-    searchNameFilt = q.searchNameTerm ? // Данные поискового термина для генерации фильтра
-        [{key: blogFields.name, value: q.searchNameTerm, way: 1}] : [],
+    sortBy = q.sortBy, // Задание исходного значения поля сортировки
+    searchNameFilt = q.searchNameTerm ? [{key: blogFields.name, value: q.searchNameTerm, way: 1}] : [], // Данные поискового термина для генерации фильтра
     [totalCount, blogs] = await blogsQueryRep.readAll(elemsSkip, q.pageSize, sortBy, q.sortDirection, searchNameFilt); // Получение сетевых журналов и их количества
     
     res.json(paginator(q.pageNumber, q.pageSize, totalCount, blogs.map(blogMaper))); // Нумерация страниц
