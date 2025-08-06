5e5f6c56a9b2129474656b5531839723b80b6a47
TvSpichkin <ericlen@mail.ru>
2025-08-06 12:35:18 +0300

    Связка контролёра с репозиторием для получения искомого сетевого журнала

--- src/routes/blogs/controllers/findBlogController.ts
+++ src/routes/blogs/controllers/findBlogController.ts
@@ -1,9 +1,13 @@
 import {Response} from "express";
 import {ReqParam} from "../../types/reqTypes";
 import {BlogIdModel, BlogViewModel} from "../types/blogsTypes";
-import {blogsServ} from "../../../domain/blogsServ";
+import {blogsQueryRep} from "../../../db/repository/blogs/blogsQueryRep";
+import {blogMaper} from "../../../db/mapers/blogMaper";
 
 
 export async function findBlogController(req: ReqParam<BlogIdModel>, res: Response<BlogViewModel>) {
-    res.json(await blogsServ.maper(res.locals.findBlog)); // Получение искомого сетевого журнала
+    const blog = await blogsQueryRep.read(+req.params.id); // Поиск сетевого журнала
+    
+    if(blog) res.json(blogMaper(blog)); // Получение искомого сетевого журнала
+    else res.sendStatus(404); // Если не найдено, то возрат 404 статуса
 } // Контролёр, отвечающий за выдачу искомого сетевого журнала
