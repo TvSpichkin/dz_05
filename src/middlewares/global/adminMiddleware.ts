ee2a912ad6316d81107d55c38d4c6cdefd2a0302
TvSpichkin <ericlen@mail.ru>
2025-08-08 12:56:02 +0300

    Добавление проверки правильности авторизации

---
+++ src/middlewares/global/adminMiddleware.ts
@@ -0,0 +1,23 @@
+import {Request, Response, NextFunction} from "express";
+import {SET} from "../../settings";
+
+
+export function fromUTF8ToBase64(code: string) {
+    return btoa(unescape(encodeURIComponent(code)));
+} // Кодирование строки в base64
+
+export function adminMiddleware(req: Request, res: Response, next: NextFunction) {
+    const auth = req.headers["authorization"]; // Получение заголовка авторизации из запроса
+    
+    function unauth() {
+        res.sendStatus(401);
+    } // Отправка статуса ошибки о попытке несанкционированного доступа
+    
+    if(!auth || (auth.slice(0, 6) !== "Basic ")) unauth(); // Проверка на базовую авторизацию
+    else {
+        const codedAuth = fromUTF8ToBase64(SET.ADMIN); // Получение base64 строки авторизации
+        
+        if(auth.slice(6) !== codedAuth) unauth(); // Сравнение строк base64
+        else next(); // Передача управления дальше
+    }
+} // Проверка правильности авторизации
