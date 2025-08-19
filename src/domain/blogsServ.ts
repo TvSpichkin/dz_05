ad2801761ed11bc9fc34d7aa3c58190bd5267f61
TvSpichkin <ericlen@mail.ru>
2025-08-19 21:18:05 +0300

    Добавление удаления сетевого журнала в предметной области

--- src/domain/blogsServ.ts
+++ src/domain/blogsServ.ts
@@ -18,4 +18,8 @@ export const blogsServ = {
         
         return newBlog;
     }, // Создание сетевого журнала
+    async del(id: string) {
+        //await repBD.remove("posts", "blogId", +id);
+        await blogsRepDB.remove(+id);
+    }, // Удаление сетевого журнала и всех его записей
 }; // Изменение сетевых журналов
