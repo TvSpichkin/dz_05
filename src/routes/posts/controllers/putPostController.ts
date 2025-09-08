fe4b0313f74bea54bc2be526d06480f34862346f
TvSpichkin <ericlen@mail.ru>
2025-09-08 16:17:45 +0300

    Добавление контролёра, отвечающего за изменение выбранной записи

---
+++ src/routes/posts/controllers/putPostController.ts
@@ -0,0 +1,10 @@
+import {Response} from "express";
+import {ReqParamBody} from "../../types/reqTypes";
+import {PostIdModel, PostInputModel} from "../types/postsTypes";
+import {postsServ} from "../../../domain/postsServ";
+
+
+export async function putPostController(req: ReqParamBody<PostIdModel, PostInputModel>, res: Response) {
+    if(await postsServ.put(req.body, +req.params.id)) res.sendStatus(204); // Отправка успешного состояния «нет содержимого»
+    else res.sendStatus(404); // Если не найдено, то возрат 404 статуса
+} // Контролёр, отвечающий за изменение выбранной записи
