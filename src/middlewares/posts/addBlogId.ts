73cd303a589cd86e9f494f4fdcaf60df08cb4f46
TvSpichkin <ericlen@mail.ru>
2025-09-09 16:10:47 +0300

    Добавление идентификатора текущего сетевого журнала при создании записи

---
+++ src/middlewares/posts/addBlogId.ts
@@ -0,0 +1,10 @@
+import {Response, NextFunction} from "express";
+import {ReqParamBody} from "../../routes/types/reqTypes";
+import {BlogIdModel} from "../../routes/blogs/types/blogsTypes";
+import {PostInputModel} from "../../routes/posts/types/postsTypes";
+
+
+export function addBlogId(req: ReqParamBody<BlogIdModel, PostInputModel>, res: Response, next: NextFunction) {
+    req.body.blogId = req.params.id; // Добавление найденного идентификатора
+    next(); // Передача управления дальше
+} // Добавление идентификатора текущего сетевого журнала при создании записи
