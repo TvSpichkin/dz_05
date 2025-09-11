444e538216eadd49d606b4b9c0a6086fa957ba43
TvSpichkin <ericlen@mail.ru>
2025-09-11 11:43:53 +0300

    Добавление набора проверок для создания и изменения записей

--- src/middlewares/posts/postValidators.ts
+++ src/middlewares/posts/postValidators.ts
@@ -16,3 +16,26 @@ async function checkExistBlog(blogId: string) {
     }
     else return Promise.reject(); // Возврат обещания
 } // Проверка существования заданного сетевого журнала
+
+const titleValidator = body("title").isString().withMessage("Название не является строкой")
+        .trim().isLength({min: 1, max: SET.MaxLen.POST.TITLE})
+        .withMessage("Название содержит больше " + SET.MaxLen.POST.TITLE + " символов или является пустым"), // Проверка правильности входящего названия
+    shortDescriptionValidator = body("shortDescription").isString().withMessage("Краткое описание не является строкой")
+        .trim().isLength({min: 1, max: SET.MaxLen.POST.ShortDescription})
+        .withMessage("Краткое описание содержит больше " + SET.MaxLen.POST.ShortDescription + " символов или является пустым"), // Проверка правильности входящего краткого описания
+    contentValidator = body("content").isString().withMessage("Содержание не является строкой")
+        .trim().isLength({min: 1, max: SET.MaxLen.POST.CONTENT})
+        .withMessage("Содержание содержит больше " + SET.MaxLen.POST.CONTENT + " символов или является пустым"), // Проверка правильности входящего содержания
+    blogIdValidator = body("blogId").isString().withMessage("Идентификатор сетевого журнала не является строкой")
+        .trim().custom(checkExistBlog).withMessage("Сетевого журнала, с введённым идентификатором, не существует"); // Проверка правильности входящего идентификатора сетевого журнала
+
+export const postValidators = [
+    adminMiddleware,
+    
+    titleValidator,
+    shortDescriptionValidator,
+    contentValidator,
+    blogIdValidator,
+    
+    inputCheckErrorsMiddleware
+]; // Набор проверок для создания и изменения записей
