f0798addcd0c75f866380afdfa7a99fa10957e43
TvSpichkin <ericlen@mail.ru>
2025-09-15 12:38:34 +0300

    Добавление маршрутизатора тестирования

---
+++ src/routes/testing/index.ts
@@ -0,0 +1,10 @@
+import express, {Response} from "express";
+
+
+export const testRout = express.Router(); // Объявление маршрутизатора тестирования
+
+testRout.delete('/all-data', async (req, res: Response) => {
+    await setDB(); // Очистка базы данных для начала тестирования
+    
+    res.sendStatus(204); // Отправка успешного состояния «нет содержимого»
+}); // Удаление всего содержимого БД
