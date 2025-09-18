be9a216714e3d68dc71205b96778e34da03c2beb
TvSpichkin <ericlen@mail.ru>
2025-09-18 16:02:04 +0300

    Добавление вспомагательных функций для тестирования

---
+++ __tests__/helpers/test-helpers.ts
@@ -0,0 +1,27 @@
+import {agent} from "supertest";
+import {app} from "../../src/app";
+import {SET} from "../../src/settings";
+import {Paginator} from "../../src/routes/present/paginator";
+
+
+export const req = agent(app), // Определение запроса для тестирования программы
+    getBlog = req.get(SET.PATH.BLOGS), // Запрос на получение всех сетевых журналов
+    getPost = req.get(SET.PATH.POSTS); // Запрос на получение всех записей
+
+export function queryBlog(q: string = ""): typeof getBlog {
+    return req.get(SET.PATH.BLOGS + "?" + q);
+} // Запрос на получение всех сетевых журналов с вопросом
+
+export function queryPost(q: string = ""): typeof getPost {
+    return req.get(SET.PATH.POSTS + "?" + q);
+} // Запрос на получение всех записей с вопросом
+
+export function pageData<T>(i: T[] = [], p: number = 1, ps: number = 10, tc: number = i.length): Paginator<T> {
+    return {
+        pagesCount: Math.ceil(tc/ps), // Количество страниц
+        page: p, // Номер текущей страницы
+        pageSize: ps, // Размер страницы - количество элементов на одной странице
+        totalCount: tc, // Количество элементов
+        items: i // Нужная порция сущностей
+    };
+} // Данные страницы
