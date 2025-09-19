9d773bd54ad20668cb5a6ab553e6d1ffc1d9904a
TvSpichkin <ericlen@mail.ru>
2025-09-19 17:41:55 +0300

    Добавление конвертации всех записей из ОЗУ БД в модельный вид для тестирования

---
+++ __tests__/helpers/maperDBM.ts
@@ -0,0 +1,10 @@
+import {DBType} from "../../src/db/repository/types/typesRepDB";
+import {PostViewModel} from "../../src/routes/posts/types/postsTypes";
+import {postMaper} from "../../src/db/mapers/postMaper";
+
+
+export const maperDBM = {
+    posts(dbm: DBType): PostViewModel[] {
+        return dbm.posts.map(p => ({...p, blogName: dbm.blogs.find(b => b.id == p.blogId)!.name})).map(postMaper);
+    } // Метод для записей
+}; // Конвертация всех сущностей из ОЗУ БД в модельный вид
