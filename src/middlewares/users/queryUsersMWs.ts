d827699fb92e47d289e936b2e480b4de656ab51e
TvSpichkin <ericlen@mail.ru>
2025-09-25 16:15:32 +0300

    Создание набора обработчиков запросов с вопросом для пользователей

---
+++ src/middlewares/users/queryUsersMWs.ts
@@ -0,0 +1,32 @@
+import {Response, NextFunction} from "express";
+import {QueryUserInputModel} from "../../routes/users/types/queryUserTypes";
+import {chStr, queryGetMiddleware} from "../global/queryGetMiddleware";
+import {userFields} from "../../routes/users/types/usersTypes";
+import {ReqQuery} from "../../routes/types/reqTypes";
+import {SetLen} from "../settingsLength";
+import {adminMiddleware} from "../global/adminMiddleware";
+
+
+function checkSB(sb: QueryUserInputModel["sortBy"]): boolean {
+    return chStr(sb) && userFields.hasOwnProperty(sb);
+} // Проверка правильности входящего поля сортировки
+
+function queryGetUsersMW(req: ReqQuery<QueryUserInputModel>, res: Response, next: NextFunction) {
+    const q = req.query;
+    
+    if(!checkSB(q.sortBy)) q.sortBy = userFields.createdAt; // Задание исходного значения поля сортировки
+    
+    if(typeof q.searchLoginTerm === "string") {
+        const slt = q.searchLoginTerm.trim();
+        if(!slt || slt.length > SetLen.Max.USER.LOGIN) q.searchLoginTerm = undefined; // Задание исходного значения поискового термина
+    } // Проверка правильности входящего поискового термина для входа
+    
+    if(typeof q.searchEmailTerm === "string") {
+        const set = q.searchEmailTerm.trim();
+        if(!set || set.length > SetLen.Max.USER.EMAIL) q.searchEmailTerm = undefined; // Задание исходного значения поискового термина
+    } // Проверка правильности входящего поискового термина для электронной почты
+    
+    next(); // Передача управления дальше
+} // Обработка запросов с вопросом на правильные значения
+
+export const queryUsersMWs = [adminMiddleware, queryGetMiddleware, queryGetUsersMW]; // Набор обработчиков запросов с вопросом для пользователей
--- src/routes/users/types/queryUserTypes.ts
+++ src/routes/users/types/queryUserTypes.ts
@@ -4,6 +4,6 @@ import {TypeUserFields} from "./usersTypes";
 
 export type QueryUserInputModel = QueryInputModel & {
     sortBy: TypeUserFields, // Входящее поле сортировки
-    searchLoginTerm: TypeSearchTerm, // Входящий поисковый термин для входа; максимальная длина: 10, минимальная длина: 3, шаблон: ^[a-zA-Z0-9_-]*$, должен быть уникальным
-    searchEmailTerm: TypeSearchTerm // Входящий поисковый термин для почты; шаблон: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$, должна быть уникальной
+    searchLoginTerm: TypeSearchTerm, // Входящий поисковый термин для входа; максимальная длина: 10
+    searchEmailTerm: TypeSearchTerm // Входящий поисковый термин для почты; максимальная длина: 254
 }; // Входная модель пользователя для запроса с вопросом
