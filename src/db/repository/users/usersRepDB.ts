9b1d2df5cae8a364147aedf363685aec5ab12ffe
TvSpichkin <ericlen@mail.ru>
2025-10-02 20:04:49 +0300

    Добавление работы с базой данных на изменение для пользователей

---
+++ src/db/repository/users/usersRepDB.ts
@@ -0,0 +1,23 @@
+import {db} from "../../db";
+import {KeysDB} from "../../types/typesRepDB";
+import {UserDbType} from "../../types/usersDbTypes";
+
+
+const entKey: KeysDB = "users";
+
+export const usersRepDB = {
+    async check(id: number): Promise<boolean> {
+        return db.collection<UserDbType>(entKey).find({id: id}).hasNext();
+    }, // Проверка на существование пользователя в БД
+    async write(entity: UserDbType): Promise<number> {
+        const endId = await db.collection<UserDbType>(entKey).find({}).sort({$natural: -1}).limit(1).toArray();
+        
+        entity.id = endId.length ? endId[0].id + 1 : 1;
+        await db.collection<UserDbType>(entKey).insertOne(entity);
+        
+        return entity.id;
+    }, // Запись пользователя в БД
+    async remove(id: number) {
+        await db.collection<UserDbType>(entKey).deleteOne({id: id});
+    } // Удаление пользователя из БД
+}; // Работа с базой данных для пользователей
