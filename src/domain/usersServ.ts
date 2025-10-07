2c14bcb02608e7fe8be386667f5bdcf636dfc17f
TvSpichkin <ericlen@mail.ru>
2025-10-07 21:43:50 +0300

    Добавление проверки на уникальность имени пользователя и адреса электронной почты при создании пользователя

--- src/domain/usersServ.ts
+++ src/domain/usersServ.ts
@@ -2,16 +2,20 @@ import {UserInputModel, userFields} from "../routes/users/types/usersTypes";
 import {UserDbType, userDbFields} from "../db/types/usersDbTypes";
 import {ProtoFilterType} from "../db/types/typesRepDB";
 import {usersRepDB} from "../db/repository/users/usersRepDB";
+import {DomResObj} from "./domTypes/resObjType";
 
 
 export const usersServ = {
-    async create(user: UserInputModel): Promise<UserDbType> {
+    async create(user: UserInputModel): Promise<DomResObj<UserDbType>> {
         const findUser = await usersRepDB.readByPF([
             {key: userDbFields.login, value: user.login, way: 0}, // Проверка уникальности для имени пользователя
             {key: userDbFields.email, value: user.email, way: 0} // Проверка уникальности для адреса электронной почты
         ]); // Поиск пользователя по текущей паре значений
         
-        // if(findUser) return {};
+        if(findUser) return {
+            isSuccess: false,
+            errField: user.email == findUser.email ? userFields.email : userFields.login
+        }; // Возврат ошибки и неуникального поля в случае совпадения
         
         const newUser: UserDbType = {
             id: 0,
@@ -24,7 +28,7 @@ export const usersServ = {
         
         newUser.id = (await usersRepDB.write(newUser));
         
-        return newUser;
+        return {isSuccess: !0, ent: newUser};
     }, // Создание пользователя
     async del(id: number): Promise<boolean> {
         const isExist = await usersRepDB.check(id);
