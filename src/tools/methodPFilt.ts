24532a15bb16dfa3b8ffbbd8df1ab43b0bf0888e
TvSpichkin <ericlen@mail.ru>
2025-10-08 22:47:16 +0300

    Добавление функции получения исходных данных для генерации фильтра

---
+++ src/tools/methodPFilt.ts
@@ -0,0 +1,6 @@
+import {ProtoFilterType} from "../db/types/typesRepDB";
+
+
+export function getPFilt<T>(k: ProtoFilterType<T>["key"], v: ProtoFilterType<T>["value"], w: ProtoFilterType<T>["way"]): ProtoFilterType<T> {
+    return {key: k, value: v, way: w};
+} // Получение исходных данных для генерации фильтра
