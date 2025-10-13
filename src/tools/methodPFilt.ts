78843e11a8d73aa5693da1db9e013da6b27ccb22
TvSpichkin <ericlen@mail.ru>
2025-10-13 13:10:35 +0300

    Добавление функции получения массива нескольких исходных данных для генерации фильтра

--- src/tools/methodPFilt.ts
+++ src/tools/methodPFilt.ts
@@ -4,3 +4,7 @@ import {ProtoFilterType} from "./types/typePFilt";
 export function getPFilt<T>(k: ProtoFilterType<T>["key"], v: ProtoFilterType<T>["value"], w: ProtoFilterType<T>["way"]): ProtoFilterType<T> {
     return {key: k, value: v, way: w};
 } // Получение исходных данных для генерации фильтра
+
+export function getSomePFilt<T>(...arg: Parameters<typeof getPFilt<T>>[]): ProtoFilterType<T>[] {
+    return arg.map(pf => getPFilt(...pf));
+} // Получение массива нескольких исходных данных для генерации фильтра
