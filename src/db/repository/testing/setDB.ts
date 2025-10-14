6ea83d98d38fc4cf3cacff26d97759918390afc2
TvSpichkin <ericlen@mail.ru>
2025-10-14 15:52:06 +0300

    В функции перезаписи БД добавление работы с пользователями

--- src/db/repository/testing/setDB.ts
+++ src/db/repository/testing/setDB.ts
@@ -1,13 +1,15 @@
 import {DBType} from "../../types/typesRepDB";
-import {blogsColl, postsColl} from "../../db";
+import {blogsColl, postsColl, usersColl} from "../../db";
 
 
 export async function setDB(dataset?: DBType) {
     await blogsColl.drop(); // Отчистка массива сетевых журналов
     await postsColl.drop(); // Отчистка массива записей
-    // Если в функцию ничего не передано - то очищаем базу данных
+    await usersColl.drop(); // Отчистка массива пользователей
+    // Если в функцию ничего не передано - то просто очищаем базу данных
     if(dataset) { // Если что-то передано - то заменяем старые значения новыми
-        if(dataset.blogs.length) await blogsColl.insertMany(dataset.blogs);
-        if(dataset.posts.length) await postsColl.insertMany(dataset.posts);
+        if(dataset.blogs.length) await blogsColl.insertMany(dataset.blogs); // Запись массива сетевых журналов
+        if(dataset.posts.length) await postsColl.insertMany(dataset.posts); // Запись массива записей
+        if(dataset.users.length) await usersColl.insertMany(dataset.users); // Запись массива пользователей
     }
 } // Функция перезаписи БД
