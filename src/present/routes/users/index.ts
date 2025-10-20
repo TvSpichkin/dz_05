290b81ff6f89ff841f6c69ac1c7cff6220c65288
TvSpichkin <ericlen@mail.ru>
2025-10-20 21:21:08 +0300

    Связка всех контролёров в маршрутизаторе пользователей

--- src/present/routes/users/index.ts
+++ src/present/routes/users/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import {queryUsersMWs} from "../../../middlewares/users/queryUsersMWs";
 import {getUsersController} from "./controllers/getUsersController";
+import {userValidators} from "../../../middlewares/users/userValidators";
+import {createUserController} from "./controllers/createUserController";
+import {idNaturalVal} from "../../../middlewares/global/idNaturalVal";
+import {adminMiddleware} from "../../../middlewares/global/adminMiddleware";
+import {delBlogController} from "../blogs/controllers/delBlogController";
 
 
 export const usersRout = express.Router(); // Объявление маршрутизатора пользователей
 
 usersRout.get("/", ...queryUsersMWs, getUsersController); // Возврат всех пользователей
-//usersRout.post("/", ...userValidators, createUserController); // Создание пользователя
+usersRout.post("/", ...userValidators, createUserController); // Создание пользователя
+usersRout.delete("/:id", idNaturalVal, adminMiddleware,  delBlogController); // Удаление пользователя
