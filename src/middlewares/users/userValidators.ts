c6f2d9ef7a5feb4264451342905afb3497fb9acd
TvSpichkin <ericlen@mail.ru>
2025-10-25 00:24:23 +0300

    Обновление проверок для создания и изменения пользователей

--- src/middlewares/users/userValidators.ts
+++ src/middlewares/users/userValidators.ts
@@ -5,14 +5,14 @@ import {adminMiddleware} from "../global/adminMiddleware";
 import {inputCheckErrorsMiddleware} from "../global/inputCheckErrorsMiddleware";
 
 
-const loginValidator = body("login").isString().withMessage("Вход не является строкой")
+export const loginValidator = (l: string) => body(l).isString().withMessage("Вход не является строкой")
         .trim().isLength({min: SetLen.Min.USER.LOGIN, max: SetLen.Max.USER.LOGIN})
         .withMessage("Вход содержит больше " + SetLen.Max.USER.LOGIN + " символов или является пустым")
         .matches(SetRE.USER.LOGIN).withMessage("Строка содержит неверные символы"), // Проверка правильности входа
     passwordValidator = body("password").isString().withMessage("Пароль не является строкой")
         .trim().isLength({min: SetLen.Min.USER.PASSWORD, max: SetLen.Max.USER.PASSWORD})
         .withMessage("Пароль содержит больше " + SetLen.Max.USER.PASSWORD + " символов или является пустым"), // Проверка правильности входящего пароля
-    emailValidator = body("email").isString().withMessage("Адрес электронной почты не является строкой")
+    emailValidator = (e: string) => body(e).isString().withMessage("Адрес электронной почты не является строкой")
         .trim().isLength({min: SetLen.Min.USER.EMAIL, max: SetLen.Max.USER.EMAIL})
         .withMessage("Адрес электронной почты содержит больше " + SetLen.Max.USER.EMAIL + " символов или является пустым")
         .matches(SetRE.USER.EMAIL).withMessage("Строка не является адресом электронной почты"); // Проверка правильности входящей электронной почты
@@ -20,9 +20,9 @@ const loginValidator = body("login").isString().withMessage("Вход не яв�
 export const userValidators = [
     adminMiddleware,
     
-    loginValidator,
+    loginValidator("login"),
     passwordValidator,
-    emailValidator,
+    emailValidator("email"),
     
     inputCheckErrorsMiddleware
 ]; // Набор проверок для создания и изменения пользователей
