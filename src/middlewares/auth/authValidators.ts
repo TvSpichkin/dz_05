d65536a9015beb1cd2b96daa71b4694559fdbaa4
TvSpichkin <ericlen@mail.ru>
2025-10-26 00:54:17 +0300

    Добавление проверки правильности входа или входящей электронной почты

---
+++ src/middlewares/auth/authValidators.ts
@@ -0,0 +1,14 @@
+import {ReqBody} from "../../present/types/reqTypes";
+import {LoginInputModel} from "../../present/routes/auth/types/authTypes";
+import {Response, NextFunction} from "express";
+import {matchedData} from "express-validator";
+import {loginValidator, passwordValidator, emailValidator} from "../users/userValidators";
+import {inputCheckErrorsMiddleware} from "../global/inputCheckErrorsMiddleware";
+
+
+async function loginOrEmailValidator(req: ReqBody<LoginInputModel>, res: Response, next: NextFunction) {
+    if(matchedData(req).loginOrEmail.includes('@')) await emailValidator("loginOrEmail").run(req);
+    else await loginValidator("loginOrEmail").run(req); // Условная развилка в зависимости от содержания символа '@'
+    
+    next(); // Передача управления дальше
+} // Проверка правильности входа или входящей электронной почты
--- src/present/routes/auth/index.ts
+++ src/present/routes/auth/index.ts
@@ -3,4 +3,4 @@ import express from "express";
 
 export const authRout = express.Router(); // Объявление маршрутизатора проверки подлинности пользователя
 
-//authRout.post("/", ...userValidators, createUserController); // Проверка подлинности пользователя
+//authRout.post("/", ...authValidators, createUserController); // Проверка подлинности пользователя
