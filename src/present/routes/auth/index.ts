efc2927df734202e7326f62316367ae341bfe5e1
TvSpichkin <ericlen@mail.ru>
2025-10-31 13:22:26 +0300

    Связка всех контролёров в маршрутизаторе проверки подлинности пользователя

--- src/present/routes/auth/index.ts
+++ src/present/routes/auth/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import {authValidators} from "../../../middlewares/auth/authValidators";
+import {loginAuthController} from "./controllers/loginAuthController";
 
 
 export const authRout = express.Router(); // Объявление маршрутизатора проверки подлинности пользователя
 
-//authRout.post("/", ...authValidators, loginAuthController); // Проверка подлинности пользователя
+authRout.post("/", ...authValidators, loginAuthController); // Проверка подлинности пользователя
