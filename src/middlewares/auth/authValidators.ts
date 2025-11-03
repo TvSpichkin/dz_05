57a15d355f86dbd2b36d7a6433d7a3a92b881ddd
TvSpichkin <ericlen@mail.ru>
2025-11-03 07:42:14 +0300

    Исправление ошибки при проверке подлинности пользователя

--- src/middlewares/auth/authValidators.ts
+++ src/middlewares/auth/authValidators.ts
@@ -1,13 +1,12 @@
 import {ReqBody} from "../../present/types/reqTypes";
 import {LoginInputModel} from "../../present/routes/auth/types/authTypes";
 import {Response, NextFunction} from "express";
-import {matchedData} from "express-validator";
 import {loginValidator, passwordValidator, emailValidator} from "../users/userValidators";
 import {inputCheckErrorsMiddleware} from "../global/inputCheckErrorsMiddleware";
 
 
 async function loginOrEmailValidator(req: ReqBody<LoginInputModel>, res: Response, next: NextFunction) {
-    if(matchedData(req).loginOrEmail.includes('@')) await emailValidator("loginOrEmail").run(req);
+    if(req.body.loginOrEmail.includes('@')) await emailValidator("loginOrEmail").run(req);
     else await loginValidator("loginOrEmail").run(req); // Условная развилка в зависимости от содержания символа '@'
     
     next(); // Передача управления дальше
